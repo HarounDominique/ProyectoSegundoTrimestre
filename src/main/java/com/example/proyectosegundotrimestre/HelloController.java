3a89dda940fddb40410a8ea80bdbf41e3d6eb24d
HAROUN <haroundominique@gmail.com>
2023-05-13 18:51:29 +0200

    vista de los datos completada, siguiente: módulos

--- src/main/java/com/example/proyectosegundotrimestre/HelloController.java
+++ src/main/java/com/example/proyectosegundotrimestre/HelloController.java
@@ -3,6 +3,7 @@ package com.example.proyectosegundotrimestre;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
@@ -11,7 +12,23 @@ public class HelloController {
     private Button cancelarButton;
 
     @FXML
+    private TextField nombreTextField;
+    @FXML
+    private TextField apellidosTextField;
+    @FXML
+    private TextField nifTextField;
+    @FXML
+    private TextField codigoPostalTextField;
+    @FXML
+    private TextField correoElectronicoTextField;
+
+    @FXML
     protected void onCancelarButtonClick() {
         //limpiar contenido de text fields
+        nombreTextField.setText("");
+        apellidosTextField.setText("");
+        nifTextField.setText("");
+        codigoPostalTextField.setText("");
+        correoElectronicoTextField.setText("");
     }
 }
